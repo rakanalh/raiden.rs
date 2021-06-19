e1775f5bc852fd40d078e062145acf069896d313
Rakan Alhneiti <rakan.alhneiti@gmail.com>
2021-06-19 09:54:12 +0300

    Use thiserror

--- src/blockchain/proxies/error.rs
+++ src/blockchain/proxies/error.rs
@@ -6,24 +6,12 @@ pub enum ProxyError {
     InsufficientEth(String),
     #[error("Broken precondition: `{0}`")]
     BrokenPrecondition(String),
-    #[error("Blockchain error: `{0}`")]
-    Web3(web3::Error),
-    #[error("Blockchain error: `{0}`")]
-    ChainError(web3::contract::Error),
+    #[error(transparent)]
+    Web3(#[from] web3::Error),
+    #[error(transparent)]
+    ChainError(#[from] web3::contract::Error),
     #[error("Recoverable error: `{0}`")]
     Recoverable(String),
     #[error("Unrecoverable error: `{0}`")]
     Unrecoverable(String),
-}
-
-impl From<web3::Error> for ProxyError {
-    fn from(e: web3::Error) -> Self {
-        Self::Web3(e)
-    }
-}
-
-impl From<web3::contract::Error> for ProxyError {
-    fn from(e: web3::contract::Error) -> Self {
-        Self::ChainError(e)
-    }
 }
