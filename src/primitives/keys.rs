57eabfdb94c3ba76e325c16b1a11a4949c245c14
Rakan Alhneiti <rakan.alhneiti@gmail.com>
2022-03-08 11:59:45 +0300

    Update web3

--- src/primitives/keys.rs
+++ src/primitives/keys.rs
@@ -42,6 +42,22 @@ impl Key for PrivateKey {
         })
     }
 
+    fn sign_message(&self, message: &[u8]) -> Result<signing::Signature, signing::SigningError> {
+        let signature = self
+            .inner
+            .sign(message)
+            .map_err(|_| signing::SigningError::InvalidMessage)?;
+
+        let standard_v = signature.v as u64;
+        let v = standard_v + 27;
+
+        Ok(signing::Signature {
+            r: H256::from(signature.r),
+            s: H256::from(signature.s),
+            v,
+        })
+    }
+
     fn address(&self) -> web3::types::Address {
         Address::from(self.inner.public().address())
     }
