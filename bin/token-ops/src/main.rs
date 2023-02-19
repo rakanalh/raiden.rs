d5cdfe273219b4526ea391f9cc60a0f3a32dd71d
Rakan Alhneiti <rakan.alhneiti@gmail.com>
2023-02-19 20:27:19 +0300

    Add approve and user-deposit subcommands

--- bin/token-ops/src/main.rs
+++ bin/token-ops/src/main.rs
@@ -42,6 +42,8 @@ pub struct Opt {
 #[derive(StructOpt, Debug)]
 enum Command {
 	Mint { amount: u64 },
+	Approve { spender: String, amount: u64 },
+	UserDeposit { contract_address: String, amount: u64 },
 }
 
 #[tokio::main]
@@ -81,8 +83,8 @@ async fn main() {
 			process::exit(1);
 		},
 	};
-	let account = Account::new(web3.clone(), private_key, nonce);
-	let proxy_manager = match ProxyManager::new(web3, contracts_manager) {
+	let account = Account::new(web3.clone(), private_key.clone(), nonce);
+	let proxy_manager = match ProxyManager::new(web3.clone(), contracts_manager) {
 		Ok(proxy_manager) => proxy_manager,
 		Err(e) => {
 			eprintln!("Error creating contracts proxy: {}", e);
@@ -98,13 +100,7 @@ async fn main() {
 		},
 	};
 
-	match token_proxy
-		.balance_of(
-			Address::from_str("0x1B74935E78F33695962c9ac278127335A4089882").expect("123"),
-			None,
-		)
-		.await
-	{
+	match token_proxy.balance_of(private_key.address(), None).await {
 		Ok(balance) => println!("Balance: {}", balance),
 		Err(e) => eprintln!("Could not get balance {}", e),
 	};
@@ -118,6 +114,76 @@ async fn main() {
 				eprintln!("Error minting token: {}", e);
 				process::exit(1);
 			},
+		},
+		Command::Approve { spender, amount } => {
+			let password =
+				rpassword::prompt_password_stderr("Password:").expect("Failed to get password");
+
+			if let Err(e) =
+				web3.personal().unlock_account(private_key.address(), &password, None).await
+			{
+				eprintln!("Could not unlock account: {}", e);
+				process::exit(1);
+			}
+
+			let spender = Address::from_str(&spender).expect("Could not parse spender");
+			match token_proxy.approve(account, spender, amount.into()).await {
+				Ok(hash) => {
+					println!("Transaction sent: {}", hash);
+				},
+				Err(e) => {
+					eprintln!("Error approving token: {}", e);
+					process::exit(1);
+				},
+			}
+		},
+		Command::UserDeposit { contract_address, amount } => {
+			let user_deposit_address =
+				Address::from_str(&contract_address).expect("Could not parse user deposit address");
+
+			match token_proxy.allowance(private_key.address(), user_deposit_address, None).await {
+				Ok(approvals) => println!("Approvals: {:?}", approvals),
+				Err(e) => eprintln!("Could not get balance {}", e),
+			};
+
+			let user_deposit = match proxy_manager.user_deposit(user_deposit_address).await {
+				Ok(user_deposit_proxy) => user_deposit_proxy,
+				Err(e) => {
+					eprintln!("Error creating user_deposit contract: {}", e);
+					process::exit(1);
+				},
+			};
+
+			let total_deposit = user_deposit
+				.total_deposit(private_key.address(), None)
+				.await
+				.expect("Could not fetch user deposit total_deposit");
+			let new_total_deposit = total_deposit.saturating_add(amount.into());
+
+			println!("New user deposit: {:?}", new_total_deposit);
+
+			let password =
+				rpassword::prompt_password_stderr("Password:").expect("Failed to get password");
+
+			if let Err(e) =
+				web3.personal().unlock_account(private_key.address(), &password, None).await
+			{
+				eprintln!("Could not unlock account: {}", e);
+				process::exit(1);
+			}
+
+			match user_deposit
+				.deposit(account, private_key.address(), new_total_deposit.into())
+				.await
+			{
+				Ok(hash) => {
+					println!("Transaction sent: {}", hash);
+				},
+				Err(e) => {
+					eprintln!("Error approving token: {}", e);
+					process::exit(1);
+				},
+			}
 		},
 	}
 }
