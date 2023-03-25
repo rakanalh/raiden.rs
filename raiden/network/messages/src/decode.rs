70771d32d35bee0dc5a25e2fc319cd2e458829bf
Rakan Alhneiti <rakan.alhneiti@gmail.com>
2023-03-26 01:46:45 +0300

    Return error on decode failure

--- raiden/network/messages/src/decode.rs
+++ raiden/network/messages/src/decode.rs
@@ -373,50 +373,58 @@ impl MessageDecoder {
 
 		match message_type {
 			"LockedTransfer" => {
-				let locked_transfer: LockedTransfer = serde_json::from_str(&body).unwrap();
+				let locked_transfer: LockedTransfer = serde_json::from_str(&body)
+					.map_err(|e| format!("Could not parse LockedTransfer message: {:?}", e))?;
 				return Ok(IncomingMessage {
 					message_identifier: locked_transfer.message_identifier,
 					inner: crate::messages::MessageInner::LockedTransfer(locked_transfer),
 				})
 			},
 			"LockExpired" => {
-				let lock_expired: LockExpired = serde_json::from_str(&body).unwrap();
+				let lock_expired: LockExpired = serde_json::from_str(&body)
+					.map_err(|e| format!("Could not parse LockExpired message: {:?}", e))?;
 				return Ok(IncomingMessage {
 					message_identifier: lock_expired.message_identifier,
 					inner: crate::messages::MessageInner::LockExpired(lock_expired),
 				})
 			},
 			"SecretRequest" => {
-				let secret_request: SecretRequest = serde_json::from_str(&body).unwrap();
+				let secret_request: SecretRequest = serde_json::from_str(&body)
+					.map_err(|e| format!("Could not parse SecretRequest message: {:?}", e))?;
 				return Ok(IncomingMessage {
 					message_identifier: secret_request.message_identifier,
 					inner: crate::messages::MessageInner::SecretRequest(secret_request),
 				})
 			},
 			"RevealSecret" => {
-				let secret_reveal: SecretReveal = serde_json::from_str(&body).unwrap();
+				let secret_reveal: SecretReveal = serde_json::from_str(&body)
+					.map_err(|e| format!("Could not parse RevealSecret message: {:?}", e))?;
 				return Ok(IncomingMessage {
 					message_identifier: secret_reveal.message_identifier,
 					inner: crate::messages::MessageInner::SecretReveal(secret_reveal),
 				})
 			},
 			"Unlock" => {
-				let unlock: Unlock = serde_json::from_str(&body).unwrap();
+				let unlock: Unlock = serde_json::from_str(&body)
+					.map_err(|e| format!("Could not parse Unlock message: {:?}", e))?;
 				return Ok(IncomingMessage {
 					message_identifier: unlock.message_identifier,
 					inner: crate::messages::MessageInner::Unlock(unlock),
 				})
 			},
 			"WithdrawRequest" => {
-				let withdraw_request: WithdrawRequest = serde_json::from_str(&body).unwrap();
+				let withdraw_request: WithdrawRequest = serde_json::from_str(&body)
+					.map_err(|e| format!("Could not parse WithdrawRequest message: {:?}", e))?;
 				return Ok(IncomingMessage {
 					message_identifier: withdraw_request.message_identifier,
 					inner: crate::messages::MessageInner::WithdrawRequest(withdraw_request),
 				})
 			},
 			"WithdrawConfirmation" => {
-				let withdraw_confirmation: WithdrawConfirmation =
-					serde_json::from_str(&body).unwrap();
+				let withdraw_confirmation: WithdrawConfirmation = serde_json::from_str(&body)
+					.map_err(|e| {
+						format!("Could not parse WithdrawConfirmation message: {:?}", e)
+					})?;
 				return Ok(IncomingMessage {
 					message_identifier: withdraw_confirmation.message_identifier,
 					inner: crate::messages::MessageInner::WithdrawConfirmation(
@@ -425,21 +433,24 @@ impl MessageDecoder {
 				})
 			},
 			"WithdrawExpired" => {
-				let withdraw_expired: WithdrawExpired = serde_json::from_str(&body).unwrap();
+				let withdraw_expired: WithdrawExpired = serde_json::from_str(&body)
+					.map_err(|e| format!("Could not parse WithdrawExpired message: {:?}", e))?;
 				return Ok(IncomingMessage {
 					message_identifier: withdraw_expired.message_identifier,
 					inner: crate::messages::MessageInner::WithdrawExpired(withdraw_expired),
 				})
 			},
 			"Processed" => {
-				let processed: Processed = serde_json::from_str(&body).unwrap();
+				let processed: Processed = serde_json::from_str(&body)
+					.map_err(|e| format!("Could not parse Processed message: {:?}", e))?;
 				return Ok(IncomingMessage {
 					message_identifier: processed.message_identifier,
 					inner: crate::messages::MessageInner::Processed(processed),
 				})
 			},
 			"Delivered" => {
-				let delivered: Delivered = serde_json::from_str(&body).unwrap();
+				let delivered: Delivered = serde_json::from_str(&body)
+					.map_err(|e| format!("Could not parse Delivered message: {:?}", e))?;
 				return Ok(IncomingMessage {
 					message_identifier: delivered.delivered_message_identifier,
 					inner: crate::messages::MessageInner::Delivered(delivered),
