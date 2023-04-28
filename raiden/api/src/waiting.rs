a73c7a5f30e8a2a40d4dec511358585b76bc49b6
Rakan Alhneiti <rakan.alhneiti@gmail.com>
2023-04-28 14:39:48 +0300

    Waiting module to reread channel state

--- raiden/api/src/waiting.rs
+++ raiden/api/src/waiting.rs
@@ -71,22 +71,18 @@ pub async fn wait_for_new_channel(
 		Some(timeout) => Duration::from_millis(timeout),
 		None => Duration::from_millis(DEFAULT_RETRY_TIMEOUT),
 	};
-	let chain_state = state_manager.read().current_state.clone();
-	let mut channel_state = views::get_channel_state_for(
-		&chain_state,
-		registry_address,
-		token_address,
-		partner_address,
-	);
 
-	while let None = channel_state {
-		sleep(retry_timeout).await;
-		channel_state = views::get_channel_state_for(
+	loop {
+		let chain_state = state_manager.read().current_state.clone();
+		if let Some(_) = views::get_channel_state_for(
 			&chain_state,
 			registry_address,
 			token_address,
 			partner_address,
-		);
+		) {
+			break
+		}
+		sleep(retry_timeout).await;
 	}
 
 	Ok(())
@@ -194,37 +190,32 @@ pub async fn wait_for_participant_deposit(
 		.map(|t| Duration::from_millis(t))
 		.unwrap_or(Duration::from_millis(DEFAULT_RETRY_TIMEOUT));
 
-	let chain_state = state_manager.read().current_state.clone();
-	let mut channel_state = match views::get_channel_state_for(
-		&chain_state,
-		registry_address,
-		token_address,
-		partner_address,
-	) {
-		Some(channel_state) => channel_state,
-		None =>
-			return Err(ApiError::State(format!(
-				"No channel could be found between provided partner and target addresses"
-			))),
-	};
-
-	let balance = if target_address == chain_state.our_address {
-		|channel_state: &ChannelState| channel_state.our_state.contract_balance
-	} else {
-		|channel_state: &ChannelState| channel_state.partner_state.contract_balance
-	};
-
-	let mut current_balance = balance(channel_state);
-	while current_balance < target_balance {
-		sleep(retry_timeout).await;
-		channel_state = views::get_channel_state_for(
+	loop {
+		let chain_state = state_manager.read().current_state.clone();
+		let channel_state = match views::get_channel_state_for(
 			&chain_state,
 			registry_address,
 			token_address,
 			partner_address,
-		)
-		.unwrap();
-		current_balance = balance(channel_state);
+		) {
+			Some(channel_state) => channel_state,
+			None =>
+				return Err(ApiError::State(format!(
+					"No channel could be found between provided partner and target addresses"
+				))),
+		};
+
+		let balance = if target_address == chain_state.our_address {
+			|channel_state: &ChannelState| channel_state.our_state.contract_balance
+		} else {
+			|channel_state: &ChannelState| channel_state.partner_state.contract_balance
+		};
+
+		let current_balance = balance(channel_state);
+		if current_balance >= target_balance {
+			break
+		}
+		sleep(retry_timeout).await;
 	}
 
 	Ok(())
