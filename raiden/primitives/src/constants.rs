44826578ad56924f1939ce2be10ef59b51d68784
Rakan Alhneiti <rakan.alhneiti@gmail.com>
2023-05-21 20:36:15 +0300

    Fix MS reward amount

--- raiden/primitives/src/constants.rs
+++ raiden/primitives/src/constants.rs
@@ -27,7 +27,7 @@ lazy_static! {
 	pub static ref EMPTY_SIGNATURE: Bytes = Bytes(vec![0; 65]);
 	pub static ref LOCKSROOT_OF_NO_LOCKS: Locksroot = Locksroot::from_slice(&keccak256(&[]));
 	pub static ref MONITORING_REWARD: TokenAmount =
-		TokenAmount::from(80 * 10).pow(TokenAmount::from(18));
+		TokenAmount::from(10).pow(TokenAmount::from(18)) * TokenAmount::from(80);
 	pub static ref PFS_DEFAULT_MAX_PATHS: usize = 3;
 	pub static ref PFS_DEFAULT_MAX_FEE: TokenAmount =
 		TokenAmount::from(10).pow(TokenAmount::from(16)).mul(TokenAmount::from(5));
